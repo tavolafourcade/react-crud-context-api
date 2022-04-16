8ad0190927dffbbf8129ab6b8d4c584afce807e2
tavolafourcade <tavolafourcade@gmail.com>
2022-04-16 17:33:02 -0500

    [ ADD ] Creating and storing a new Post

--- server/controllers/posts.controllers.js
+++ server/controllers/posts.controllers.js
@@ -1,10 +1,25 @@
 import Post from '../models/Post.js'
+
 // Return an array of posts from the DB
 export const getPosts = async (req, res) => {
   const posts = await Post.find()
   res.send(posts)
 }
-export const createPost = (req, res) => res.send('New post created')
+
+// Create a post
+export const createPost = async (req, res) => {
+  const { title, description } = req.body
+
+  // Creating a new Post in our DB
+  const newPost = new Post({ title, description })
+
+  // Saving the new Post
+  await newPost.save()
+
+// Answering to the Client with a json object (a new Post)
+  return res.json(newPost)
+}
+
 export const updatePost = (req, res) => res.send('Updating a post')
 export const deletePost = (req, res) => res.send('Deleting a post')
 export const getPost = (req, res) => res.send('Getting a post')
