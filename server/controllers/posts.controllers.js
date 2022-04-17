43aa3713c440d50c88f1fad2daa969147e710a06
tavolafourcade <tavolafourcade@gmail.com>
2022-04-16 19:31:27 -0500

    [ ADD ] Error Handling for Controllers

--- server/controllers/posts.controllers.js
+++ server/controllers/posts.controllers.js
@@ -2,44 +2,63 @@ import Post from '../models/Post.js'
 
 // Return an array of posts from the DB
 export const getPosts = async (req, res) => {
-  const posts = await Post.find()
-  res.send(posts)
+  try {
+    const posts = await Post.find()
+    return res.send(posts)
+  } catch (error) {
+    return res.status(500).json({ message: error.message })
+  }
 }
 
 // Create a post
 export const createPost = async (req, res) => {
-  const { title, description } = req.body
+  try {
+    const { title, description } = req.body
 
-  // Creating a new Post in our DB
-  const newPost = new Post({ title, description })
+    // Creating a new Post in our DB
+    const newPost = new Post({ title, description })
 
-  // Saving the new Post
-  await newPost.save()
+    // Saving the new Post
+    await newPost.save()
 
-  // Answering to the Client with a json object (a new Post)
-  return res.json(newPost)
+    // Answering to the Client with a json object (a new Post)
+    return res.json(newPost)
+  } catch (error) {
+    return res.status(500).json({ message: error.message })
+  }
 }
 
 // Update a post
 export const updatePost = async (req, res) => {
-  const updatedPost = await Post.findByIdAndUpdate(req.params.id, req.body, { new: true })
-
-  updatedPost.save()
-  return res.send(updatedPost)
+  try {
+    const updatedPost = await Post.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    updatedPost.save()
+    return res.send(updatedPost)
+  } catch (error) {
+    return res.status(500).json({ message: error.message })
+  }
 }
 
 // Delete a post
 export const deletePost = async (req, res) => {
-  const postRemoved = await Post.findByIdAndDelete(req.params.id)
+  try {
+    const postRemoved = await Post.findByIdAndDelete(req.params.id)
 
-  if (!postRemoved) return res.sendStatus(404)
-  return res.sendStatus(204)
+    if (!postRemoved) return res.sendStatus(404)
+    return res.sendStatus(204)
+  } catch (error) {
+    return res.status(500).json({ message: error.message })
+  }
 }
 
 // Return one post from the DB
 export const getPost = async (req, res) => {
-  const postRequested = await Post.findById(req.params.id)
+  try {
+    const postRequested = await Post.findById(req.params.id)
 
-  if (!postRequested) return res.sendStatus(404)
-  return res.json(postRequested)
+    if (!postRequested) return res.sendStatus(404)
+    return res.json(postRequested)
+  } catch (error) {
+    return res.status(500).json({ message: error.message })
+  }
 }
