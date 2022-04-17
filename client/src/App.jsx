c33d4e67e699c9673f5c28f6ebae2ebf9b2f3ef4
tavolafourcade <tavolafourcade@gmail.com>
2022-04-17 15:44:53 -0500

    [ ADD ] Setting basic structure for components [ FIX ] Issue with Eslint

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { HomePage, NotFoundPage, PostForm } from './pages'
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+      <NotFoundPage />
+      <PostForm />
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/pages/PostForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PostForm() {
+  return (
+    <div>Formulario</div>
+  )
+}
+
+export default PostForm
