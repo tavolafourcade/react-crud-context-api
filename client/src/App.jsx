d5a4c884e3e78761c88ef3fd96ad29739d427032
tavolafourcade <tavolafourcade@gmail.com>
2022-04-17 16:45:13 -0500

    [ ADD ] Tailwind setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,15 @@ import { HomePage, NotFoundPage, PostForm } from './pages'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/new" element={<PostForm />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+    <div className="bg-neutral-900 min-h-screen flex items-center">
+      <div className="px-10 container m-auto">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/new" element={<PostForm />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
+    </div>
   )
 }
 
